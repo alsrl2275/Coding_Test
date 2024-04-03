8a9e5540df2b8338f23486de292d24df1c4dc801
alsrl2275 <142866976+alsrl2275@users.noreply.github.com>
2024-04-03 15:55:34 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.11 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer;
+        if((s.length() == 4 || s.length() == 6) && s.matches("^[0-9]+$")) {
+        	answer = true;												 
+        } else {															
+        	answer = false;							
+        }
+        return answer;
+    }
+}
